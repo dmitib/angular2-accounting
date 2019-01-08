84bc3476908d51ed0e2fced912de5ff91eb2bddb
tib <d.gushin@corp.nn.ru>
2019-01-09 02:54:02 +0300

    add validation to login form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,16 +5,18 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ],
   declarations: [
     LoginComponent,
     RegistrationComponent,
     AuthComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    SharedModule
   ],
   providers: []
 })
